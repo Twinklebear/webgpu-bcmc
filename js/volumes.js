9a7a32a7766a081cc94b21cb02ba6cd278b33abd
Will Usher <will@willusher.io>
2023-01-16 11:34:05 -0800

    finalize volumes for benchmarks

--- js/volumes.js
+++ js/volumes.js
@@ -38,13 +38,6 @@ var datasets = {
         range: [550, 2100],
         scale: [1, 1, 1],
     },
-    tacc_turbulence: {
-        compressionRate: 2,
-        name: "tacc_turbulence_256x256x256_float32.raw.crate2",
-        range: [0, 10],
-        scale: [1, 1, 1],
-        step: 10 / 100,
-    },
     duct: {
         compressionRate: 2,
         name: "duct_193x194x1000_float32.raw.crate2",
@@ -52,32 +45,12 @@ var datasets = {
         scale: [1, 1, 1000 / 193],
         step: 4 / 100
     },
-    beechnut: {
-        compressionRate: 1,
-        name: "beechnut_1024x1024x1546_uint16.raw.crate1",
-        range: [13200, 17000],
-        scale: [1, 1, 1],
-        step: (17000 - 13200) / 100.0,
-    },
-    jicf_q: {
+    stagbeetle: {
         compressionRate: 2,
-        name: "jicf_q_1408x1080x1100_float32.raw.crate2",
-        range: [-15, 15],
+        name: "stag_beetle_832x832x494_uint16.raw.crate2",
+        range: [100, 4096],
         scale: [1, 1, 1],
-        step: 30 / 100
-    },
-    truss: {
-        compressionRate: 2,
-        name: "synthetic_truss_with_five_defects_1200x1200x1200_float32.raw.crate2",
-        range: [0, 0.01],
-        scale: [1, 1, 1],
-    },
-    // For benchmarks:
-    skull: {
-        compressionRate: 2,
-        name: "skull_256x256x256_uint8.raw.crate2",
-        range: [10, 255],
-        scale: [1, 1, 1],
+        step: 1.0 / 4096,
     },
     foot: {
         compressionRate: 2,
@@ -92,19 +65,26 @@ var datasets = {
         scale: [1, 1, 1],
         step: 1.0 / 3800.0,
     },
+    // For benchmarks:
+    tacc_turbulence: {
+        compressionRate: 2,
+        name: "tacc_turbulence_256x256x256_float32.raw.crate2",
+        range: [0, 10],
+        scale: [1, 1, 1],
+        step: 10 / 100,
+    },
+    skull: {
+        compressionRate: 2,
+        name: "skull_256x256x256_uint8.raw.crate2",
+        range: [10, 255],
+        scale: [1, 1, 1],
+    },
     magnetic: {
         compressionRate: 4,
         name: "magnetic_reconnection_512x512x512_float32.raw.crate4",
         range: [0.1, 3.5],
         scale: [1, 1, 1],
         step: 1.0 / 8192,
-    },
-    stagbeetle: {
-        compressionRate: 2,
-        name: "stag_beetle_832x832x494_uint16.raw.crate2",
-        range: [100, 4096],
-        scale: [1, 1, 1],
-        step: 1.0 / 4096,
     },
     kingsnake: {
         compressionRate: 2,
@@ -119,12 +99,32 @@ var datasets = {
         scale: [1, 1, 1],
         step: 1.0 / 8192,
     },
+    beechnut: {
+        compressionRate: 1,
+        name: "beechnut_1024x1024x1546_uint16.raw.crate1",
+        range: [13200, 17000],
+        scale: [1, 1, 1],
+        step: (17000 - 13200) / 100.0,
+    },
     miranda: {
         compressionRate: 4,
         name: "miranda_1024x1024x1024_float32.raw.crate4",
         range: [1.05, 2.9],
         scale: [1, 1, 1],
         step: 1.0 / 8192,
+    },
+    jicf_q: {
+        compressionRate: 2,
+        name: "jicf_q_1408x1080x1100_float32.raw.crate2",
+        range: [-15, 15],
+        scale: [1, 1, 1],
+        step: 30 / 100
+    },
+    truss: {
+        compressionRate: 2,
+        name: "synthetic_truss_with_five_defects_1200x1200x1200_float32.raw.crate2",
+        range: [0, 0.01],
+        scale: [1, 1, 1],
     },
     dns_large: {
         compressionRate: 2,
@@ -137,7 +137,7 @@ var datasets = {
         compressionRate: 1,
         name: "richtmyer_meshkov_2048x2048x1920_uint8.raw.crate1",
         range: [0, 255],
-        scale: [1, 1, 1920 / 2048],
+        scale: [1, 1, 1920.0 / 2048.0],
         step: 1.0,
     },
 };
@@ -153,3 +153,4 @@ function runBenchmark(benchmark)
 {
     requestBenchmark = benchmark;
 }
+
