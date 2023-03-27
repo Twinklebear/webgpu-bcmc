2df67bb98e8e218897f9451c135bed627e759a24
Will Usher <will@willusher.io>
2023-03-27 09:09:03 -0700

    explicitly request maxBufferSize the device can do

--- js/render.js
+++ js/render.js
@@ -6,6 +6,7 @@
         requiredLimits: {
             maxStorageBuffersPerShaderStage: adapter.limits.maxStorageBuffersPerShaderStage,
             maxStorageBufferBindingSize: adapter.limits.maxStorageBufferBindingSize,
+            maxBufferSize: adapter.limits.maxBufferSize
         },
     };
     var device = await adapter.requestDevice(gpuDeviceDesc);
